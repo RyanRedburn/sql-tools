82c7ff9bbf7a607be8b73a83eefb6d063389f9bd
RyanRedburn <44010201+RyanRedburn@users.noreply.github.com>
2018-10-29 21:35:53 -0400

    Update Analysis/TableSpaceUsed.sql
    Reworked TableSpaceUsed to include more information, have more precise space stats, and include tables with clustered columnstore indexes.

--- Analysis/TableSpaceUsed.sql
+++ Analysis/TableSpaceUsed.sql
@@ -1,15 +1,25 @@
 SET NOCOUNT ON;
 
-SELECT t.[name] AS table_name, s.[name] AS [schema_name], p.[rows] AS row_count,
-    (SUM(a.total_pages) * 8) / 1024 AS total_space_in_mb, (SUM(a.used_pages) * 8) / 1024 AS used_space_in_mb,
-    ((SUM(a.total_pages) - SUM(a.used_pages)) * 8) / 1024 AS unused_space_in_mb
-FROM sys.tables AS t
-	JOIN sys.indexes AS i ON t.[object_id] = i.[object_id]
-	JOIN sys.partitions AS p ON i.[object_id] = p.[object_id] AND i.index_id = p.index_id
-	JOIN sys.allocation_units AS a ON p.[partition_id] = a.container_id
-	LEFT JOIN sys.schemas AS s ON t.[schema_id] = s.[schema_id]
-WHERE t.[name] NOT LIKE 'dt%'
-    AND t.is_ms_shipped = 0
-    AND i.[object_id] > 255
-GROUP BY t.[name], s.[name], p.[rows]
-ORDER BY total_space_in_mb DESC, used_space_in_mb DESC, table_name;
+WITH result([object_id], [schema_id], table_type, row_count, partition_count, total_space_in_mb, used_space_in_mb, unused_space_in_mb)
+AS
+(
+	SELECT t.[object_id], t.[schema_id], i.[type_desc], SUM(ps.row_count), COUNT(DISTINCT ps.partition_number),
+		CAST(ROUND((SUM(a.total_pages) * 8) / 1024.00, 2) AS DECIMAL(18,2)), CAST(ROUND((SUM(a.used_pages) * 8) / 1024.00, 2) AS DECIMAL(18,2)),
+		CAST(ROUND(((SUM(a.total_pages) - SUM(a.used_pages)) * 8) / 1024.00, 2) AS DECIMAL(18,2))
+	FROM sys.tables AS t
+		JOIN sys.schemas AS s ON s.[schema_id] = t.[schema_id]
+		JOIN sys.indexes AS i ON i.[object_id] = t.[object_id]
+		JOIN sys.dm_db_partition_stats AS ps ON ps.[object_id] = i.[object_id]
+			AND ps.index_id = i.index_id
+		JOIN sys.allocation_units AS a ON a.container_id = ps.[partition_id]
+	WHERE t.[type] = 'U'
+		AND t.is_ms_shipped = 0
+		AND i.[type] IN (0, 1, 5)
+	GROUP BY t.[object_id], t.[schema_id], i.[type_desc])
+SELECT s.[name] AS [schema_name], t.[name] AS table_name, r.table_type, r.row_count, r.partition_count,
+	r.total_space_in_mb, r.used_space_in_mb, r.unused_space_in_mb, t.lock_escalation_desc, t.is_replicated,
+	t.is_published, t.is_merge_published, t.is_sync_tran_subscribed, t.is_tracked_by_cdc, t.is_filetable
+FROM result AS r
+	JOIN sys.tables AS t ON t.[object_id] = r.[object_id]
+	JOIN sys.schemas AS s ON s.[schema_id] = t.[schema_id]
+ORDER BY s.[name], t.[name];
